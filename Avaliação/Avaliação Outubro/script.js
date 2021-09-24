12fec7fa6f8ad9e2f721a19ef1a1a0d33f0fdaa2
Flavio Valle Da Cunha <88721496+flavio-valle@users.noreply.github.com>
2021-09-24 15:09:13 -0300

    Add files via upload

---
+++ Avaliação/Avaliação Outubro/script.js
@@ -0,0 +1,14 @@
+var num = parseInt (prompt ("digite um número"));
+
+if (num < 0) {
+    alert ("seu número é negativo");
+
+}
+else if (num == 0 ) {
+    alert ("seu número é zero");
+
+}
+
+else if (num > 0) {
+    alert ("seu número é positivo");
+}
